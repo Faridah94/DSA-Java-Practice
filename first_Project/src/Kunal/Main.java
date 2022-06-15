71858f465d22de4da8e4a0c67a3f1ef2765be8c0
Faridah Alabsi <faridah.absi@gmail.com>
2022-06-15 23:39:01 +0300

    this is the first tutorial

---
+++ first_Project/src/Kunal/Main.java
@@ -0,0 +1,24 @@
+package Kunal;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("helloWorld");
+//
+//        Scanner input = new Scanner(System.in);
+//        float num = input.nextFloat(); // you can enter integer number to the float and it will turned automatically into float using promotion and type casting
+//        System.out.println(num);
+
+//        int num = (int)43.4354356;
+//        System.out.println(num);
+
+        byte a = 40;
+        byte b = 50;
+        byte c = 100;
+        int d = a * b / c;
+        System.out.println(d);
+    }
+}
+
